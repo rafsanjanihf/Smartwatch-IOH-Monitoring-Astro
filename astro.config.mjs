98b23c324b37d00b8004dd163e8e8b453359bea3
Dawam Raja <akunnyadawam9@gmail.com>
2025-02-18 09:42:17 +0700

    adding cloudfare adadptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import react from '@astrojs/react';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind(), react(), sentry(), spotlightjs()],
   output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
 });
