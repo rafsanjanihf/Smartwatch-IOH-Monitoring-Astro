b9549c8a88e27bb72af29ccca54b9d82e12668e2
Dawam Raja <akunnyadawam9@gmail.com>
2025-02-18 09:54:10 +0700

    hold ondeman render

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,7 @@ import react from '@astrojs/react';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   integrations: [tailwind(), react(), sentry(), spotlightjs()],
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
 });
