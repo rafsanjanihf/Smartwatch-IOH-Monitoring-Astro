322d15b159c251742b60461bd34510025a32b56b
Rafsanjani H.F <araf235@gmail.com>
2025-07-29 16:09:26 +0700

    Update Wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from '@astrojs/react';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind(), react(), sentry(), spotlightjs()],
+  adapter: cloudflare(),
 });
