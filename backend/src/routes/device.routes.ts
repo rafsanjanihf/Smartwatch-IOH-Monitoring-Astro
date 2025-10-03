5f5679ff63eb9b79393d75da0c151224bc228628
Rafsanjani H.F <araf235@gmail.com>
2025-10-03 11:16:07 +0700

    migrate backend to another repo

--- backend/src/routes/device.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getDevices, getDeviceById } from '../controllers/device.controller';
-
-const router = Router();
-
-router.get('/', getDevices);
-router.get('/:id', getDeviceById);
-
-export default router;
--- backend/src/routes/userDevice.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getUserDevices, getUserDeviceById } from '../controllers/userDevice.controller';
-
-const router = Router();
-
-router.get('/', getUserDevices);
-router.get('/:id', getUserDeviceById);
-
-export default router;
